a6aaa76f59cf489bc28dde2c34144f9f78157879
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-07-31 01:16:35 -0700

    Pink_mode

---
+++ script.js
@@ -0,0 +1,20 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This contect added by JavaScript";
+
+document.querySelector("body").appendChild(h2);
+
+const body = document.querySelector("body");
+
+function togglePinkMode() {
+  body.classList.toggle("pink-mode");
+  const pinkModeEnabled = body.classList.contains("pink-mode");
+  localStorage.setItem("pinkMode", pinkModeEnabled);
+}
+
+const pinkModePref = localStorage.getItem("pinkMode");
+if (pinkModePref !== null) {
+  body.classList.toggle("pink-mode", pinkModePref === "true");
+}
+
+const pinkModeToggle = document.getElementById("pinkModeToggle");
+pinkModeToggle.addEventListener("click", togglePinkMode);
