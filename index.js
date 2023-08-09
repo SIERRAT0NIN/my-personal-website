aa9f25c1975c0451110385c5c1a0db1b5f143f69
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-08-08 21:27:09 -0700

    jquery test

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// To add element and appendChild to HTML
 const p = document.createElement("p");
 p.textContent = "FlatIron School";
 
@@ -5,17 +6,19 @@ document.querySelector("body").appendChild(p);
 
 const body = document.querySelector("body");
 
+// Pink Mode
+// Function=>add class to body to toggle "pink-mode" on and off.
 function togglePinkMode() {
   body.classList.toggle("pink-mode");
+  // saving to local storage. I do not understand yet.
   const pinkModeEnabled = body.classList.contains("pink-mode");
   localStorage.setItem("pinkMode", pinkModeEnabled);
 }
-
 const pinkModePref = localStorage.getItem("pinkMode");
 if (pinkModePref !== null) {
   body.classList.toggle("pink-mode", pinkModePref === "true");
 }
-
+// adding event listener to Pink Mode button, to toggle class "pink-mode"
 const pinkModeToggle = document.getElementById("pinkModeToggle");
 pinkModeToggle.addEventListener("click", togglePinkMode);
 
@@ -23,17 +26,31 @@ pinkModeToggle.addEventListener("click", togglePinkMode);
 const apiKey = "b6ba3ed28ec0a2ed6183145e16564d56";
 const city = "San Francisco"; // Replace with the name of your desired city
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-// Fetch
+// Fetch api data from apiUrl
 fetch(apiUrl)
   .then((response) => response.json())
   .then((data) => {
     const weatherData = document.getElementById("weatherData");
+    // innerHTML?
     weatherData.innerHTML = `
           <p>City: ${data.name}</p>
-          <p>Temperature: ${data.main.temp.description}°F</p>
+          <p>Temperature: ${data.main.temp}°C</p>
           <p>Weather: ${data.weather[0].description}</p>
         `;
   })
   .catch((error) => {
     console.error("Error fetching weather data:", error);
   });
+
+//   $(document).ready(function() {
+//     $("#toggleButton").click(function() {
+//         $("#toggleText").toggle();
+//     });
+// });
+// $("input").keypress(function(event){
+//   $("h1).text(event.key);
+// })
+
+$("h1").on("mouseover", function () {
+  $("h1").css("color", " purple");
+});
